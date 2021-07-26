e3b1e7486a3cb743e595697ed043ae36af690b6a
Hok S. Uy <hoksuy@gmail.com>
2021-07-25 19:32:23 -0700

    json parsing functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,15 @@ var forecast = "forecast?q=";
 function lookupWeather(query) {
     fetch(endpoint + weather + query + weatherAPIkey).then(function(weatherResponse) {
         if(weatherResponse.ok) {
-            return weatherResponse.json;
+            return weatherResponse.json();
         } else {
             alert(weatherResponse.status);
         }
     }).then(function(weatherData) {
-        console.log(weatherData);
+        outputWeather(weatherData);
         fetch(endpoint + forecast + query + weatherAPIkey).then(function(forecastResponse) {
             if(forecastResponse.ok) {
-                return(forecastResponse.json);
+                return forecastResponse.json();
             } else {
                 alert(forecastResponse.status);
             }
@@ -23,6 +23,53 @@ function lookupWeather(query) {
         });
     });
 };
+
+function outputWeather(data) {
+    var cardEl = $("#main-card");
+    cardEl.html("");
+
+    var cardBodyEl = $("<div class='card-body'>");
+    var h5El = $("<div class='card-title'>");
+    var h6El = $("<div class='card-subtitle mb-2 text-muted'>");
+    
+
+    var date = new Date(data.dt * 1000);
+    date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+    h5El.text(data.name + " (" + date + ")");
+    h5El.appendTo(cardBodyEl);
+    
+    for(var i = 0; i < 4; i++) {
+        var pEl = $("<p>");
+        var lineText = "";
+
+        switch(i) {
+            case 0: lineText += "Temp: " + convertToImperial(data.main.temp);
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+        pEl.appendTo(cardBodyEl);
+    }
+
+    cardBodyEl.appendTo(cardEl);
+}
+
+function outputForecast(forecastData) {
+
+}
+
+function convertToImperial(kelvin) {
+    return (((kelvin - 273.15) * 1.8) + 32);
+}
+
+function convertToMetric(kelvin) {
+    return (kelvin - 273.15);
+}
 
 $("#search-btn").click(function() {
     var search = ($(this).parent().siblings()[0].value);
